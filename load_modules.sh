a0181350cab3be9d9d973b3a7898a8e96109bdd9
Hunter Pfannenstiel <hpfannen@eos.beocat.ksu.edu>
2024-02-24 16:29:02 -0600

    Added bash script to load modules

---
+++ load_modules.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#bash: ./load_modules.sh: Permission denied --> chmod +x load_modules.sh
+
+echo "Loading hw3 modules..."
+module load googletest/1.10.0-GCCcore-9.3.0 CMake/3.16.4-GCCcore-9.3.0 foss/2020a
+echo "Successfully loaded hw3 modules"
